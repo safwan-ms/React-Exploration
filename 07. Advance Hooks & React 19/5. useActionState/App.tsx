fa5198a2530c9f812c118768feee49ad6cb03a20
safwan-ms <safwan.anasms@gmail.com>
2025-04-15 20:27:27 +0530

    React 19 useActionState

---
+++ 07. Advance Hooks & React 19/5. useActionState/App.tsx
@@ -0,0 +1,11 @@
+import { useActionState } from "react";
+import Count from "./components/Count";
+
+const App = () => {
+  return (
+    <div>
+      <Count />
+    </div>
+  );
+};
+export default App;
